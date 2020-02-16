09fe623e8779d317fb16288f5dc2e2cc40ef1c53
burnouttt <burnouttt@yandex.ru>
2020-02-16 20:55:42 +0300

    fix

--- cameras_control.py
+++ cameras_control.py
@@ -6,4 +6,4 @@ from onvif import ONVIFCamera
 def call_default_preset(onvif_ip: str) -> None:
     user, password, host, port = re.split("[@:]", onvif_ip)
 
-    onvif_camera = ONVIFCamera(host, int(port), user, password, '/etc/onvif/wsdl/')
+    onvif_camera = ONVIFCamera(host, port, user, password, '/etc/onvif/wsdl/')
