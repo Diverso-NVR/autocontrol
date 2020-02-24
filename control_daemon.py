f85b324bd0b442e220455243934459c701f9830d
burnouttt <burnouttt@yandex.ru>
2020-02-24 19:26:48 +0300

    schedule job added

--- control_daemon.py
+++ control_daemon.py
@@ -11,6 +11,7 @@ class AutoControlApp:
 
     def __init__(self):
         schedule.every(2).minutes.do(self.call_default_presets)
+        schedule.every().day.at("00:00").do(self.set_autocontrol_true)
 
     def call_default_presets(self):
         session = Session()
@@ -24,6 +25,16 @@ class AutoControlApp:
                         goto_home_position(source.ip, source.port)
                     except Exception as e:
                         print(e)
+
+    def set_autocontrol_true(self):
+        session = Session()
+        self.rooms = session.query(Room).all()
+
+        for room in self.rooms:
+            room.auto_control = True
+
+        session.commit()
+        session.close()
 
     def run(self):
         while True:
