43c5cbd1c6d8a7a29f382dec19b4e84524ad795c
kuderr <dakudryavcev@gmail.com>
2020-10-30 17:46:18 +0300

    restart on failure

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,9 @@
-docker stop nvr_autocontrol
+failuredocker stop nvr_autocontrol
 docker rm nvr_autocontrol
 docker build -t nvr_autocontrol .
 docker run -d \
  -it \
+ --restart on-failure \
  --name nvr_autocontrol \
  --net=host \
  --env-file ../.env_nvr \
